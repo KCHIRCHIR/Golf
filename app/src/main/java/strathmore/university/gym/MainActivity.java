e8dba82a5145e353af8cb857423cb4d52efd5956
Michael Chirchir <michael.chirchir@strathmore.edu>
2020-03-31 00:14:31 +0300

    User Interface for login, register and reset password page created

--- app/src/main/java/strathmore/university/gym/MainActivity.java
+++ app/src/main/java/strathmore/university/gym/MainActivity.java
@@ -2,13 +2,41 @@ package strathmore.university.gym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mRegister;
+    TextView mReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegister = findViewById(R.id.register);
+        mReset = findViewById(R.id.forgotPassword);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ResetPassword.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/strathmore/university/gym/ResetPassword.java
+++ app/src/main/java/strathmore/university/gym/ResetPassword.java
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class ResetPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.reset_password);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        Intent intent = new Intent(ResetPassword.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
