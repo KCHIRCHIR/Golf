508b8feb2c27d87ebb4f9a5b8d9510e9ce919609
Michael Chirchir <michael.chirchir@strathmore.edu>
2020-03-31 02:38:35 +0300

    Users can now sign in successfully

--- app/src/main/java/strathmore/university/gym/MainActivity.java
+++ app/src/main/java/strathmore/university/gym/MainActivity.java
@@ -1,18 +1,34 @@
 package strathmore.university.gym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEmail, mPassword;
     TextView mRegister;
     TextView mReset;
     private Button mLogin;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference Users_94112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +36,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRegister = findViewById(R.id.register);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
         mReset = findViewById(R.id.forgotPassword);
         mLogin = findViewById(R.id.btnLogin);
+
+        // Initialize Firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        Users_94112 = db.getReference("Users_94112");
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,9 +67,32 @@ public class MainActivity extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GymLocations.class);
-                startActivity(intent);
-                finish();
+                // Check Email Validation
+                if (TextUtils.isEmpty(mEmail.getText())) {
+                    Toast.makeText(MainActivity.this, "Please Enter Your Email Address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Check Password Validation
+                if (mPassword.getText().toString().length() < 6) {
+                    Toast.makeText(MainActivity.this, "Password Is Too Short !!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Sign In
+                auth.signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(MainActivity.this, GymLocations.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Log In Error", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
             }
         });
 
