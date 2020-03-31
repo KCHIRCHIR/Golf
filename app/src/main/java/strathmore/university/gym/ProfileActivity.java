a5ef8c5112e995ba57a049821925584144962ba0
Michael Chirchir <michael.chirchir@strathmore.edu>
2020-03-31 03:11:46 +0300

    Users can now view their profile details

--- app/src/main/java/strathmore/university/gym/ProfileActivity.java
+++ app/src/main/java/strathmore/university/gym/ProfileActivity.java
@@ -1,23 +1,87 @@
 package strathmore.university.gym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private EditText mFirstName, mLastName, mEmailAddress;
+    private ImageView mProfileImage;
     private Button mSave;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgressDialog;
+    private DatabaseReference Users_94112;
+
+    private String userID;
+    private String mFirst;
+    private String mLast;
+    private String mEmail;
+    private String mProfileImageUrl;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+//        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+ //       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mFirstName = (EditText) findViewById(R.id.firstName);
+        mLastName = (EditText) findViewById(R.id.lastName);
+        mEmailAddress = (EditText) findViewById(R.id.email);
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
         mSave = findViewById(R.id.saveChanges);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        Users_94112 = FirebaseDatabase.getInstance().getReference().child("Users_94112").child(userID);
+
+        getUserInfo();
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
 
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +91,122 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void getUserInfo(){
+        Users_94112.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("firstName")!=null){
+                        mFirst = map.get("firstName").toString();
+                        mFirstName.setText(mFirst);
+                    }
+                    if(map.get("lastName")!=null){
+                        mLast = map.get("lastName").toString();
+                        mLastName.setText(mLast);
+                    }
+                    if(map.get("email")!=null){
+                        mEmail = map.get("email").toString();
+                        mEmailAddress.setText(mEmail);
+                    }
+                    if(map.get("profileImageUrl")!=null){
+                        mProfileImageUrl = map.get("profileImageUrl").toString();
+                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+            onBackPressed();
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveUserInformation() {
+        mProgressDialog.setMessage("Saving... \n Please Wait.. :)");
+        mProgressDialog.show();
+        mFirst = mFirstName.getText().toString();
+        mLast = mLastName.getText().toString();
+        mEmail = mEmailAddress.getText().toString();
+
+        Map userInfo = new HashMap();
+        userInfo.put("firstName", mFirst);
+        userInfo.put("lastName", mLast);
+        userInfo.put("email", mEmail);
+        Users_94112.updateChildren(userInfo);
+
+        if(resultUri != null) {
+
+            StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mProgressDialog.dismiss();
+                    Toast.makeText(ProfileActivity.this, "Unable to save", Toast.LENGTH_SHORT).show();
+                    finish();
+                    return;
+                }
+            });
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgressDialog.dismiss();
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful());
+                    Uri downloadUrl = urlTask.getResult();
+
+                    final String sdownload_url = String.valueOf(downloadUrl);
+
+                    Map newImage = new HashMap();
+                    newImage.put("profileImageUrl", sdownload_url);
+                    Users_94112.updateChildren(newImage);
+                    return;
+                }
+            });
+        }else{
+            finish();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
     }
 
     @Override
